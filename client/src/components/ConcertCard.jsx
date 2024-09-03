0f327a1e1f8b4c37516bc11c4b06a6a6107ac828
DrXeon <alessandromilani2000@gmail.com>
2024-09-03 22:16:15 +0200

    refactor: Update ConcertCard component to improve readability and add missing styles

--- client/src/components/ConcertCard.jsx
+++ client/src/components/ConcertCard.jsx
@@ -2,18 +2,24 @@ import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 
+
+
 const ConcertCard = (props) => {
-    const { date, theater, description, name, id} = props.concert;
+    const { date, theater, description, name, id } = props.concert;
+
+
     return (
-        <Card>
+
+        <Card style={{ height: '12rem' }}>
             <Card.Body>
                 <Card.Title>{name}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{date} at {theater}</Card.Subtitle>
+
                 <Card.Text>
-                    {description}
+                    {description.substring(0, 20) + '...' || description}
                 </Card.Text>
-                <Button variant="dark" className='w-100'>
-                    <Link style={{color: '#fff', textDecoration: 'none'}} to={`/concert/${id}`}>Go to &gt;&gt;</Link>
+                <Button variant="dark" className='mx-auto' style={{ position: 'absolute', bottom: '5px' }}>
+                    <Link style={{ color: '#fff', textDecoration: 'none' }} to={`/concert/${id}`}>Go to &gt;&gt;</Link>
                 </Button>
             </Card.Body>
         </Card>
