e38829d38ee3b02b3c65e7ac36d7a3bcc4064b64
DrXeon <alessandromilani2000@gmail.com>
2024-09-09 14:11:25 +0200

    refactor and implemented check max value Form.Control

--- client/src/components/ConcertOrder.jsx
+++ client/src/components/ConcertOrder.jsx
@@ -1,9 +1,13 @@
-import { React, useState } from 'react';
-import { Form, Button, ListGroup, Card, Toast } from 'react-bootstrap';
-import API from '../API';
-import { GenericModal } from './GenericModal';
+import { React, useState, useContext } from 'react';
 import { FaTrash } from "react-icons/fa";
 import { RiDiscountPercentFill } from "react-icons/ri";
+
+import { Form, Button, ListGroup, Card, Toast } from 'react-bootstrap';
+
+import { GenericModal } from './GenericModal';
+import UserContext from '../contexts/UserContext';
+
+import API from '../API';
 
 const ConcertOrder = (props) => {
     const [showModal, setShowModal] = useState(false);
@@ -12,84 +16,62 @@ const ConcertOrder = (props) => {
     const [showDiscountToast, setDiscountToast] = useState(false);
     const [discountMessage, setDiscountMessage] = useState('');
 
-    const { preBookedSeats, setPreBookedSeats} = props;
-    const { totalSeats,  concert} = props;
-    const { bookedSeats, setbookedSeats } = props;
+    const { preBookedSeats, setPreBookedSeats, bookedSeats, setbookedSeats, concert } = props;
     const { setAlertMessage, setShowAlert, setErrorBookedSeats } = props;
-    const { authToken, setAuthToken } = props;
+
+
+    const userContext = useContext(UserContext);
+
 
     const confirmPreBooking = (toBookSeats) => {
         API.bookSeats(concert.id, toBookSeats).then(res => {
-            setbookedSeats({
-                id: bookedSeats.id,
-                seats: [...bookedSeats.seats, ...toBookSeats]
-            });
+            setbookedSeats({ id: bookedSeats.id, seats: [...bookedSeats.seats, ...toBookSeats] });
             getDiscount(preBookedSeats.map(seat => seat.label));
             setPreBookedSeats([]);
             setErrorBookedSeats([]);
             setAlertMessage('Seats successfully booked! Go to your prenotations to see the details.');
             setShowAlert(true);
         }).catch(err => {
-            if(err.seats !== undefined){
+            if (err.seats !== undefined) {
                 setAlertMessage(`sorry, but ${err.seats.length} ${err.seats.length > 1 ? 'seats are' : 'seat is'} already booked`);
                 setErrorBookedSeats(err.seats);
                 setPreBookedSeats({ seatsId: [], seatsLabel: [] });
                 setShowAlert(true);
             }
-            else{
-            setAlertMessage('Booking failed. Please try again.');
-            setShowAlert(true);
+            else {
+                setAlertMessage('Booking failed. Please try again.');
+                setShowAlert(true);
             }
         });
     }
 
-    const getDiscount = (seats) => {
-        if (authToken) {
-            API.getDiscount(seats, authToken).then(res => {
-                setDiscountMessage("You have Won a discount " + res.discount + "% Discount!! Valid for the upcoming concert season");
-                setDiscountToast(true);
-            }).catch(err => {
-                // sistemare
-                API.getAuthToken().then(resp => setAuthToken(resp.token));
-                API.getDiscount(seats, authToken).then(res => {
-                    setShowAlert(false);
-                    setDiscountMessage("You have Won a discount " + res.discount + "% Discount!! Valid for the upcoming concert season");
-                    setDiscountToast(true);
-                }).catch(err => {
-                    setAlertMessage('Discaunt non available.');
-                    setShowAlert(true);
-                });
-            });
-        }
-    };
-
-    const handleConfirmClick = () => { setShowModal(true); };
-
-    const handleBookingClose = () => { setShowModal(false); };
-
-    const handleDiscountClose = () => {  setDiscountToast(false); };
-
-    const handlePrenotationClick = () => { setShowPrenotationModal(true); };
-
-    const handlePrenotationByserverClose = () => { setShowPrenotationModal(false); };
-
     const confirmPrenotationByServer = () => {
         setPreBookedSeats([]);
-        const availableSeats = Array.from({ length: totalSeats }, (_, i) => i + 1).filter(seat => !bookedSeats.seats.includes(seat));
-        if (numSeats > availableSeats.length) {
-            return;
-        }
+        const availableSeats = Array.from({ length: concert.total_seats }, (_, i) => i + 1).filter(seat => !bookedSeats.seats.includes(seat));
+        if (numSeats > availableSeats.length) { return; }
         confirmPreBooking(availableSeats.slice(0, numSeats));
         setShowPrenotationModal(false);
     };
 
+    const getDiscount = (seats) => {
+        if (userContext.authToken) {
+            API.getDiscount(seats, userContext.authToken).then(res => {
+                setDiscountMessage("You have Won a " + res.discount + "% discount!! Valid for the upcoming concert season");
+                setDiscountToast(true);
+            }).catch(err => {
+                setAlertMessage('Discount non available.');
+                setShowAlert(true);
+            });
+        }
+    };
 
+    // Handlers for modals and toasts
+    const handleConfirmClick = () => { setShowModal(true); };
+    const handleBookingClose = () => { setShowModal(false); };
+    const handleDiscountClose = () => { setDiscountToast(false); };
 
-    const prenotationList = (seats) => {
-        return (seats.map((seat, index) => {
-            return <ListGroup.Item key={index}>{seat.label}<span onClick={() => handleRemoveSeat(index)} style={{ float: 'right', cursor: 'pointer' }}><FaTrash className='text-danger' /></span> </ListGroup.Item>;
-        }));
-    }
+    const handlePrenotationClick = () => { setShowPrenotationModal(true); };
+    const handlePrenotationByserverClose = () => { setShowPrenotationModal(false); };
 
     const handleRemoveSeat = (index) => {
         const newSeats = [...preBookedSeats];
@@ -97,7 +79,11 @@ const ConcertOrder = (props) => {
         setPreBookedSeats(newSeats);
     }
 
-    const maxSeats = bookedSeats.seats === undefined ? totalSeats : totalSeats - bookedSeats.seats.length;
+    const prenotationList = (seats) => {
+        return (seats.map((seat, index) => {
+            return <ListGroup.Item key={index}>{seat.label}<span onClick={() => handleRemoveSeat(index)} style={{ float: 'right', cursor: 'pointer' }}><FaTrash className='text-danger' /></span> </ListGroup.Item>;
+        }));
+    }
 
     return (
         <>
@@ -109,10 +95,16 @@ const ConcertOrder = (props) => {
                             <Form.Control
                                 type="number"
                                 min="1"
-                                max={maxSeats}
-                                onChange={(e) => setNumSeats(e.target.value)}
+                                max={concert.total_seats - bookedSeats.seats.length}
+                                value={numSeats}
+                                onChange={(e) => {
+                                    if (e.target.value > 1) {
+                                        const newValue = Math.min(e.target.value, concert.total_seats - bookedSeats.seats.length);
+                                        setNumSeats(newValue);
+                                    } else { setNumSeats(e.target.value); }
+                                }}
                             />
-                            <Button variant="dark" onClick={handlePrenotationClick} className="mt-2 w-50">Make server choose seats</Button>
+                            <Button className='mt-2' variant="dark" onClick={handlePrenotationClick}>Prenotate</Button>
                         </Form.Group>
                     </Form>
                 </Card.Body>
@@ -124,7 +116,6 @@ const ConcertOrder = (props) => {
                         <Button className='w-50 mb-2' variant="dark"
                             onClick={handleConfirmClick}>Confirm selected seats
                         </Button>
-
                         <ListGroup style={{ maxHeight: '15rem', overflowY: 'scroll' }}>
                             {prenotationList(preBookedSeats)}
                         </ListGroup>
@@ -133,30 +124,27 @@ const ConcertOrder = (props) => {
                 : null}
 
             <GenericModal
-                show={showModal}
-                handleClose={handleBookingClose}
-                title="Confirm Booking"
+                show={showModal} items={preBookedSeats.map(seat => seat.label)}
+                handleClose={handleBookingClose} title="Confirm Booking"
                 bodyText="Are you sure you want to book the following seats?"
-                items={preBookedSeats.map(seat => seat.label)}
                 onConfirm={() => {
                     confirmPreBooking(preBookedSeats.map(seat => seat.id));
                     handleBookingClose();
                 }} />
 
             <GenericModal
-                show={showPrenotationModal}
-                handleClose={handlePrenotationByserverClose}
-                title="Create Prenotation"
+                show={showPrenotationModal} items={[]}
+                handleClose={handlePrenotationByserverClose} title="Create Prenotation"
                 bodyText={`Are you sure you want to create a prenotation with ${numSeats} seats? \n Alredy selected seats will be lost.`}
-                items={[]}
                 onConfirm={() => {
                     confirmPrenotationByServer();
                     handlePrenotationByserverClose();
                 }}
             />
 
-            <Toast className='position-absolute top-0 start-50 translate-middle-x mx-3 my-3' 
-                    onClose={handleDiscountClose} show={showDiscountToast} animation={true}>
+            <Toast style={{ zIndex: '200' }} delay={6000} animation={true} autohide
+                className='position-absolute top-0 start-50 translate-middle-x mx-3 my-3'
+                onClose={handleDiscountClose} show={showDiscountToast} >
                 <Toast.Header>
                     <RiDiscountPercentFill />
                     <strong className="me-auto">Discount</strong>
@@ -166,7 +154,5 @@ const ConcertOrder = (props) => {
         </>
     );
 }
-
-
 
 export { ConcertOrder };
