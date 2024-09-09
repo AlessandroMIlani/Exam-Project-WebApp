243c5cbc20d81a0f17cf36d5a89314593689d198
DrXeon <alessandromilani2000@gmail.com>
2024-09-09 14:12:55 +0200

    Added Legend for color coding

--- client/src/components/ConcertPage.jsx
+++ client/src/components/ConcertPage.jsx
@@ -2,15 +2,17 @@ import { useEffect, useState, useContext } from 'react';
 import { Row, Col, Card, Container, Alert } from 'react-bootstrap';
 
 import API from '../API';
+
 import { LoadingLayout } from './Layout';
 import { ConcertOrder } from './ConcertOrder';
-
-import "../styles/ConcertPage.css";
-
 import UserContext from '../contexts/UserContext';
 import { getSeat } from '../services/utils';
 
+import "../styles/ConcertPage.css";
+
 const ConcertPage = (props) => {
+
+  const { concertId } = props;
 
   const [concert, setConcert] = useState(null);
   const [preBookedSeats, setPreBookedSeats] = useState([]);
@@ -18,9 +20,9 @@ const ConcertPage = (props) => {
   const [ErrorBookedSeats, setErrorBookedSeats] = useState([]);
   const [showAlert, setShowAlert] = useState(false);
   const [alertMessage, setAlertMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
   const userContext = useContext(UserContext);
-  const { authToken, setAuthToken } = props;
-  const { concertId } = props;
 
   useEffect(() => {
     API.getConcertByID(concertId).then(NewConcert => {
@@ -38,30 +40,24 @@ const ConcertPage = (props) => {
   }, [concertId]);
 
 
-
-  const [isLoading, setIsLoading] = useState(true);
-
-
   const buildTheater = () => {
     return Array.from({ length: concert.rows }).map((_, i) => (
       <tr key={i}>
         {Array.from({ length: concert.columns }).map((_, j) => {
           const id = i * concert.columns + j + 1;
           const seatLabel = getSeat(id, concert.columns);
-            const seatClass = `seat ${ErrorBookedSeats.includes(id)
+          const seatClass = `seat ${ErrorBookedSeats.includes(id)
             ? 'error-booked'
             : (userContext.loggedIn && preBookedSeats.find(seat => seat.id === id))
               ? 'pre-booked'
               : (bookedSeats.seats.includes(id))
-              ? 'booked'
-              : 'available'
+                ? 'booked'
+                : 'available'
             }`;
 
           return (
             <td
-              key={id}
-              id={`seats-${id}`}
-              className={seatClass}
+              key={id} id={`seats-${id}`} className={seatClass}
               onClick={() => userContext.loggedIn && !bookedSeats.seats.includes(id) && bookSeats(id, seatLabel)}
             >
               {seatLabel}
@@ -73,11 +69,9 @@ const ConcertPage = (props) => {
   };
 
   const bookSeats = (seatId, seatLabel) => {
-    console.log('bookSeats', seatId, seatLabel);
     for (const seat in preBookedSeats) {
       if (preBookedSeats[seat].id === seatId) {
         const newPreBookedSeats = preBookedSeats.filter(seat => seat.id !== seatId);
-        console.log('newPreBookedSeats', newPreBookedSeats);
         setPreBookedSeats(newPreBookedSeats);
         return;
       }
@@ -85,17 +79,16 @@ const ConcertPage = (props) => {
     setPreBookedSeats(prevSeats => [...prevSeats, { id: seatId, label: seatLabel }]);
   };
 
-  // rename concert info
   return (
-    <Container className='mb-5'>
+    <Container className='mb-5 main-content my-3'>
 
       {isLoading
         ? <LoadingLayout />
         : <>
           <Row className='my-3'>
             <Col xs={5}>
-              <Card className='concert-card' style={{ height: '10rem' }}>
-                <Card.Body>
+              <Card className='concert-card d-flex ' style={{ minHeight: '10rem' }}>
+                <Card.Body className="align-items-stretch">
                   <Card.Title><h2>{concert.name}</h2></Card.Title>
                   <Card.Subtitle className="mb-2 text-muted">{concert.date} at {concert.theater}</Card.Subtitle>
                   <Card.Text>
@@ -105,8 +98,8 @@ const ConcertPage = (props) => {
               </Card>
             </Col>
             <Col xs={{ offset: 2, span: 5 }}>
-              <Card className='concert-card' style={{ height: '10rem' }}>
-                <Card.Body >
+              <Card className='concert-card' style={{ minHeight: '10rem'}}>
+                <Card.Body className='d-flex align-items-stretch'>
                   <table style={{ height: '100%', width: '100%' }} className='text-end'>
                     <tbody>
                       <tr className='align-middle'>
@@ -114,16 +107,27 @@ const ConcertPage = (props) => {
                         <td>{concert.total_seats}</td>
                       </tr>
                       <tr className='align-middle'>
-                        <td><strong className='legend-available'>Available Seats:</strong></td>
+                        <td><strong>Available Seats:</strong></td>
                         <td> {concert.total_seats - bookedSeats.seats.length}</td>
                       </tr>
                       <tr className='align-middle'>
-                        <td><strong className='legend-booked'>Booked Seats:</strong></td>
+                        <td><strong>Booked Seats:</strong></td>
                         <td>{bookedSeats.seats.length}</td>
                       </tr>
-                      <tr className='align-middle'>
-                        <td><strong className='legend-pre-booked'>Selected Seats:</strong></td>
-                        <td>{preBookedSeats.length}</td>
+                      {userContext.loggedIn ?
+                        <tr className='align-middle'>
+                          <td><strong>Selected Seats:</strong></td>
+                          <td>{preBookedSeats.length}</td>
+                        </tr>
+                        : null}
+                      <tr className='align-top'>
+                        <td>
+                          <Row className={`legend-container my-2`}>
+                            <Col >Available: <div className="legend-square legend-available-square"></div></Col>
+                              <Col >Booked: <div className="legend-square legend-booked-square"></div></Col>
+                              {userContext.loggedIn ? <Col>Selected: <div className="legend-square legend-pre-booked-square"></div></Col> : null}
+                          </Row>
+                        </td>
                       </tr>
                     </tbody>
                   </table>
@@ -131,12 +135,12 @@ const ConcertPage = (props) => {
               </Card>
             </Col>
           </Row>
-          <Row>
-          {showAlert && (
-        <Alert className='mt-3 fade show' variant={alertMessage.includes('successfully') ? 'success' : 'danger'} onClose={() => setShowAlert(false)} dismissible>
-          {alertMessage}
-        </Alert>
-      )}
+          <Row className='my-5'>
+            {showAlert && (
+              <Alert className='mt-3 fade show' variant={alertMessage.includes('successfully') ? 'success' : 'danger'} onClose={() => setShowAlert(false)} dismissible>
+                {alertMessage}
+              </Alert>
+            )}
             <Col className={!userContext.loggedIn ? 'd-flex justify-content-center' : null}>
               <table className="theater">
                 <tbody>
@@ -144,30 +148,23 @@ const ConcertPage = (props) => {
                 </tbody>
               </table>
             </Col>
-            <Col>
-              {userContext.loggedIn ?
+            {userContext.loggedIn ?
+              <Col>
                 <ConcertOrder
                   concert={concert}
-                  preBookedSeats={preBookedSeats}
-                  setPreBookedSeats={setPreBookedSeats}
-                  totalSeats={concert.total_seats}
-                  bookedSeats={bookedSeats}
-                  setbookedSeats={setbookedSeats}
-                  authToken={authToken}
-                  setAuthToken={setAuthToken}
-                  setShowAlert={setShowAlert}
-                  setAlertMessage={setAlertMessage}
+                  setPreBookedSeats={setPreBookedSeats} preBookedSeats={preBookedSeats}
+                  bookedSeats={bookedSeats} setbookedSeats={setbookedSeats}
+                  setShowAlert={setShowAlert} setAlertMessage={setAlertMessage}
                   setErrorBookedSeats={setErrorBookedSeats}
                 />
-                : null
-              }
-            </Col>
+              </Col>
+              : null
+            }
           </Row>
         </>
       }
     </Container>
   );
 }
-
 
 export { ConcertPage };
