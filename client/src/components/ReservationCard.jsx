13a394f5390ed230e377187604069b8da51b978e
DrXeon <alessandromilani2000@gmail.com>
2024-09-13 21:10:06 +0200

    Refactor ReservationCard component: Update variable names and fix typo

--- client/src/components/ReservationCard.jsx
+++ client/src/components/ReservationCard.jsx
@@ -3,18 +3,20 @@ import { Card, Button, Col } from 'react-bootstrap';
 import { FaTrash } from 'react-icons/fa';
 import { getSeat } from '../services/utils';
 
-const ReservationCard = ({ order, handleDelete }) => {
+const ReservationCard = ({ reserverion, handleDelete }) => {
   return (
-      <Card className='order-card'>
+    <Col xs={12} md={4} lg={3} className='mb-3' key={reserverion.id}>
+      <Card className='reserverion-card'>
         <Card.Body>
-          <Card.Title>{order.concert_name}</Card.Title>
+          <Card.Title>{reserverion.concert_name}</Card.Title>
           <Card.Text>
-            <strong>Date:</strong> {order.concert_date} <br />
-            <strong>Seats:</strong> {order.seats.map(seat => getSeat(seat, order.columns)).join(', ')}
-            <Button className='delete-button' variant="danger" size='sm' onClick={() => handleDelete(order.id)}><FaTrash /></Button>
+            <strong>Date:</strong> {reserverion.concert_date} <br />
+            <strong>Seats:</strong> {reserverion.seats.map(seat => getSeat(seat, reserverion.columns)).join(', ')}
+            <Button className='delete-button' variant="danger" size='sm' onClick={() => handleDelete(reserverion.id)}><FaTrash /></Button>
           </Card.Text>
         </Card.Body>
-      </Card>
+      </Card> 
+    </Col>
   );
 };
 
--- client/src/components/ReservationsList.jsx
+++ client/src/components/ReservationsList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Row, Col, Container } from 'react-bootstrap';
+import { Row, Container } from 'react-bootstrap';
 
 import API from '../API';
 
@@ -10,70 +10,68 @@ import { ReservationCard } from './ReservationCard';
 import '../styles/ReservationsList.css';
 
 const ReservationsList = () => {
-    const [orders, setOrders] = useState([]);
+    const [reserverions, setReserverions] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [showModal, setShowModal] = useState(false);
-    const [orderToDelete, setOrderToDelete] = useState(null);
+    const [reserverionToDelete, setReserverionToDelete] = useState(null);
 
     useEffect(() => {
-        API.getBookedSeatsByUser().then(orders => {
-            setOrders(orders);
+        API.getBookedSeatsByUser().then(reserverions => {
+            setReserverions(reserverions);
             setIsLoading(false);
         }).catch(err => {
             setIsLoading(false);
         });
     }, []);
 
-    const handleDelete = (orderId) => {
-        setOrderToDelete(orderId);
+    const handleDelete = (reserverionId) => {
+        setReserverionToDelete(reserverionId);
         setShowModal(true);
     };
 
     const confirmDelete = () => {
-        if (orderToDelete) {
-            API.deleteBookedSeat(orderToDelete)
+        if (reserverionToDelete) {
+            API.deleteBookedSeat(reserverionToDelete)
                 .then(() => {
-                    setOrders(orders.filter(order => order.id !== orderToDelete));
+                    setReserverions(reserverions.filter(reserverion => reserverion.id !== reserverionToDelete));
                     setShowModal(false);
-                    setOrderToDelete(null);
+                    setReserverionToDelete(null);
                 })
                 .catch(err => {
                     setShowModal(false);
-                    setOrderToDelete(null);
+                    setReserverionToDelete(null);
                 });
         }
     };
 
-    return (
-        <>
-            {isLoading ? (
-                <LoadingLayout />
-            ) : (
-                <Container className='main-content'>
-                    {orders.length === 0 ? (
-                        <h2 className='display-2 text-center my-2'>No reservations found.</h2>
-                    ) : (
-                        <>
-                            <h2 className='display-2 text-center my-3'>Your Reservations</h2>
-                            <Row>
-                                {orders.map(order => (
-                                    <Col xs={12} md={4} lg={3} className='mb-3' key={order.id}>
-                                        <ReservationCard order={order} handleDelete={handleDelete} />
-                                    </Col>
-                                ))}
-                            </Row>
-                        </>
-                    )}
-                </Container>
-            )}
-            <GenericModal
-                show={showModal} title="Confirm Delete"  items={[]}
-                handleClose={() => setShowModal(false)}
-                bodyText="Are you sure you want to delete this reservation?"
-                onConfirm={confirmDelete}
-            />
-        </>
-    );
+  return (
+    <>
+      {isLoading ? (
+        <LoadingLayout />
+      ) : (
+        <Container className='main-content'>
+          {reserverions.length === 0 ? (
+            <h2 className='display-2 text-center my-2'>No reservations found.</h2>
+          ) : (
+            <>
+              <h2 className='display-2 text-center my-3'>Your Reservations</h2>
+              <Row>
+                {reserverions.map(reserverion => (
+                  <ReservationCard key={reserverion.id} reserverion={reserverion} handleDelete={handleDelete} />
+                ))}
+              </Row>
+            </>
+          )}
+        </Container>
+      )}
+      <GenericModal
+        show={showModal} title="Confirm Delete" items={[]}
+        handleClose={() => setShowModal(false)}
+        bodyText="Are you sure you want to delete this reservation?"
+        onConfirm={confirmDelete}
+      />
+    </>
+  );
 };
 
 export { ReservationsList };
